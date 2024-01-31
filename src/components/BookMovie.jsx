84ddca20048d21189412b4ca476aeb7e25bdcae4
Kinshu-Learner <kinshurajgupta1@gmail.com>
2024-02-01 03:01:36 +0530

    UI fix

--- src/components/BookMovie.jsx
+++ src/components/BookMovie.jsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const MovieBookingForm = () => {
     const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        age: '',
-        phoneNumber: '',
+        name: "",
+        email: "",
+        age: "",
+        phoneNumber: "",
     });
 
     const handleInputChange = (e) => {
@@ -16,7 +16,7 @@ const MovieBookingForm = () => {
 
     const handleFormSubmit = (e) => {
         e.preventDefault();
-        localStorage.setItem('userData', JSON.stringify(formData));
+        localStorage.setItem("userData", JSON.stringify(formData));
     };
 
     const params = useParams();
@@ -29,45 +29,57 @@ const MovieBookingForm = () => {
             const fullShow = await fetch(`https://api.tvmaze.com/shows/${id}`);
             const fullS = await fullShow.json();
             setShow(fullS);
-        }
+        };
         fetchFullshow();
-    }, []);
+    }, [id]);
 
     return (
-        <div className='flex flex-col md:flex-row mt-10 bg-indigo-300 rounded-lg'>
-            <div className="md:w-1/2">
-
+        <div className="flex flex-col md:flex-row mt-10 bg-indigo-300 rounded-lg justify-between">
+            <div className="md:w-full">
                 <div className="text-2xl text-white font-bold p-3 bg-indigo-700 rounded-t-lg md:rounded-none md:rounded-tl-lg">
-                    You're booking: <span className='text-4xl text-yellow-300'> {show.name}</span>
+                    You're booking:{" "}
+                    <span className="text-4xl text-yellow-300"> {show.name}</span>
                 </div>
 
                 <div className="my-6 mx-3">
-
                     <div className="text-xl font-bold mb-2">Show Details:</div>
 
-                    {show.runtime && <div className="font-semibold">
-                        Runtime: <span className='font-bold'> {show.runtime}</span>
-                    </div>}
+                    {show.runtime && (
+                        <div className="font-semibold">
+                            Runtime: <span className="font-bold"> {show.runtime}</span>
+                        </div>
+                    )}
 
-                    {show.rating && <div className="font-semibold">
-                        Average Rating: <span className='font-bold'> {show.rating.average}</span>
-                    </div>}
+                    {show.rating && (
+                        <div className="font-semibold">
+                            Average Rating:{" "}
+                            <span className="font-bold"> {show.rating.average}</span>
+                        </div>
+                    )}
 
-                    {show.schedule && <div className="font-semibold">
-                        Schedule Time: <span className='font-bold'> {show.schedule.time}</span>
-                    </div>}
+                    {show.schedule && (
+                        <div className="font-semibold">
+                            Schedule Time:{" "}
+                            <span className="font-bold"> {show.schedule.time}</span>
+                        </div>
+                    )}
 
-                    {show.schedule && <div className="font-semibold">
-                        Schedule Days: <span className='font-bold'> {show.schedule.days}</span>
-                    </div>}
-
+                    {show.schedule && (
+                        <div className="font-semibold">
+                            Schedule Days:{" "}
+                            <span className="font-bold"> {show.schedule.days}</span>
+                        </div>
+                    )}
                 </div>
 
-                <form onSubmit={handleFormSubmit} className='flex flex-col rounded-lg p-3'>
-                    <label className='py-2 md:px-6 text-lg font-bold'>
+                <form
+                    onSubmit={handleFormSubmit}
+                    className="flex flex-col rounded-lg p-3"
+                >
+                    <label className="py-2 md:px-6 text-lg font-bold">
                         Name:
                         <input
-                            className='rounded  bg-indigo-200 focus:border-b-2 border-indigo-400 focus:outline-none px-2 py-1 mx-2 text-base font-normal'
+                            className="rounded  bg-indigo-200 focus:border-b-2 border-indigo-400 focus:outline-none px-2 py-1 mx-2 text-base font-normal"
                             type="text"
                             name="name"
                             value={formData.name}
@@ -75,10 +87,10 @@ const MovieBookingForm = () => {
                             required
                         />
                     </label>
-                    <label className='py-2 md:px-6 text-lg font-bold'>
+                    <label className="py-2 md:px-6 text-lg font-bold">
                         Email:
                         <input
-                            className='rounded  bg-indigo-200 focus:border-b-2 border-indigo-400 focus:outline-none px-2 py-1 mx-2 text-base font-normal '
+                            className="rounded  bg-indigo-200 focus:border-b-2 border-indigo-400 focus:outline-none px-2 py-1 mx-2 text-base font-normal "
                             type="email"
                             name="email"
                             value={formData.email}
@@ -86,10 +98,10 @@ const MovieBookingForm = () => {
                             required
                         />
                     </label>
-                    <label className='py-2 md:px-6 text-lg font-bold'>
+                    <label className="py-2 md:px-6 text-lg font-bold">
                         Age:
                         <input
-                            className='rounded  bg-indigo-200 focus:border-b-2 border-indigo-400 focus:outline-none px-2 py-1 mx-2 text-base font-normal right-0'
+                            className="rounded  bg-indigo-200 focus:border-b-2 border-indigo-400 focus:outline-none px-2 py-1 mx-2 text-base font-normal right-0"
                             type="number"
                             name="age"
                             value={formData.age}
@@ -97,10 +109,10 @@ const MovieBookingForm = () => {
                             required
                         />
                     </label>
-                    <label className='py-2 md:px-6 text-lg font-bold'>
+                    <label className="py-2 md:px-6 text-lg font-bold">
                         Phone Number:
                         <input
-                            className='rounded  bg-indigo-200 focus:border-b-2 border-indigo-400 focus:outline-none px-2 py-1 mx-2 text-base font-normal '
+                            className="rounded  bg-indigo-200 focus:border-b-2 border-indigo-400 focus:outline-none px-2 py-1 mx-2 text-base font-normal "
                             type="tel"
                             name="phoneNumber"
                             value={formData.phoneNumber}
@@ -108,15 +120,27 @@ const MovieBookingForm = () => {
                             required
                         />
                     </label>
-                    <button type="submit" className='text-xl font-bold bg-yellow-300 hover:bg-yellow-200 duration-200 w-40 rounded ml-6 mt-6 p-2'>Book Movie</button>
+                    <button
+                        type="submit"
+                        className="text-xl font-bold bg-yellow-300 hover:bg-yellow-200 duration-200 w-40 rounded ml-6 mt-6 p-2"
+                    >
+                        Book Movie
+                    </button>
                 </form>
             </div>
-
-            {show.image && <div className="">
-                <div className="p-4 flex mx-auto bg-indigo-800 justify-center items-center rounded-lg md:rounded-none md:rounded-r-lg">
-                    <img src={show.image && show.image.original} alt={show.name} className='overflow-hidden max-w-sm' />
-                </div>
-            </div>}
+            <div className="flex">
+                {show.image && (
+                    <div className="w-full">
+                        <div className="p-4 flex mx-auto bg-indigo-800 justify-center items-center rounded-lg md:rounded-none md:rounded-r-lg">
+                            <img
+                                src={show.image && show.image.original}
+                                alt={show.name}
+                                className="overflow-hidden max-w-sm"
+                            />
+                        </div>
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
